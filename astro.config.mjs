a8ca9b601570a9d2bc4982de21c4a62e322b8b58
hkmjtech <ajmlhkm@gmail.com>
2023-10-09 21:48:09 +0800

    fix sanity astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+// my-blog/frontend/astro.config.mjs
+
+import { defineConfig } from "astro/config";
+import sanity from "astro-sanity";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "oo2ojieb",
+      dataset: "production",
+      apiVersion: "2023-02-08",
+      useCdn: false,
+    }),
+  ],
+});
