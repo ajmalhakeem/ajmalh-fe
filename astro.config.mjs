1bd4256860660af50850f6d5ffc2e87e351aa6d1
hkmjtech <ajmlhkm@gmail.com>
2023-10-20 22:49:58 +0800

    add tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import sanity from "astro-sanity";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "oo2ojieb",
-      dataset: "production",
-      apiVersion: "2023-02-08",
-      useCdn: false,
-    }),
-  ],
+  integrations: [sanity({
+    projectId: "oo2ojieb",
+    dataset: "production",
+    apiVersion: "2023-02-08",
+    useCdn: false
+  }), tailwind(), react()]
 });
---
+++ src/components/Main.tsx
@@ -0,0 +1,25 @@
+export default function Main() {
+  return (
+    <div className="bg-white pt-24 sm:pt-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 ">
+        <div className="mx-auto max-w-2xl pb-10 border-b border-gray-200">
+          <div className="mb-5">
+            <h3 className="text-xl font-bold tracking-tight text-gray-700 sm:text-xl mb-0">
+              "Website"
+            </h3>
+            <h3 className="text-lg font-bold tracking-tight text-gray-700 sm:text-lg -mt-1">
+              by HAKEEM JAMAL
+            </h3>
+          </div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            WRITINGS 2023
+          </h2>
+          <p className="text-xs font-bold leading-8 text-gray-800">
+            A collection of blog posts that I have written this year about
+            travel, tech, and life
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
