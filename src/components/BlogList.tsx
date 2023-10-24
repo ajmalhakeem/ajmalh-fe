d3036547533022fcd8afdd0854ce305aa39710c4
hkmjtech <ajmlhkm@gmail.com>
2023-10-24 22:01:25 +0800

    add date-fns and pull categories

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,27 +1,7 @@
-const blogPosts = [
-  {
-    id: 1,
-    title: "Boost your conversion rate",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  // More posts...
-];
+import { format } from "date-fns";
 
 //TODO: Change any to correct type
 export default function BlogList({ posts }: { posts: any }) {
-  console.log(posts);
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
       <div className="mx-auto max-w-2xl pb-10 ">
@@ -33,14 +13,16 @@ export default function BlogList({ posts }: { posts: any }) {
             >
               <div className="flex items-center gap-x-4 text-xs">
                 <time dateTime={post._createdAt} className="text-gray-500">
-                  {post._createdAt}
+                  {format(new Date(post._createdAt), "d MMMM y")}
                 </time>
-                <a
-                  href={`/post/${post.slug.current}`}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {/* {post.categories[0].title} */}
-                </a>
+                {post.categoryTitle && (
+                  <a
+                    href={`/post/${post.slug.current}`}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    {post.categoryTitle}
+                  </a>
+                )}
               </div>
               <div className="group relative">
                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
